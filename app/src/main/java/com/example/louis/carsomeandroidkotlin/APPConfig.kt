1e5d7846d26e5f61f09ec95a669edabb8034d6df
limyang51 <limyang51@gmail.com>
2017-12-28 19:48:55 +0800

    done car list item layout

--- app/src/main/java/com/example/louis/carsomeandroidkotlin/APPConfig.kt
+++ app/src/main/java/com/example/louis/carsomeandroidkotlin/APPConfig.kt
@@ -1,5 +1,7 @@
 package com.example.louis.carsomeandroidkotlin
 
+import com.example.louis.carsomeandroidkotlin.model.Car
+
 /**
  * Created by Louis on 28/12/2017.
  */
@@ -10,4 +12,6 @@ object APPConfig {
 
     const val DUMMY_CREDENTIALS_ID = "test@carsome.com"
     const val DUMMY_CREDENTIALS_PASS = "qwerqwer"
+
+    val DUMMY_CAR_LIST: ArrayList<Car> = ArrayList()
 }
